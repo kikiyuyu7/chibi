4866d5408634e07977d16e8df161b2c0a99ec29a
OSHIMA Yukie <21716023@jwu.ac.jp>
2019-10-14 16:59:29 +0900

    expsecond

---
+++ exp1.py
@@ -0,0 +1,41 @@
+class Expr(object):
+    pass
+
+class Val(Expr):
+    __slots__ = ['value']
+    def __init__(self, value = 0):
+        self.value = value
+    def __repr__(self):
+        return f'Val({self.value})'
+    def eval(self):
+        return self.value
+
+v = Val(1)
+print(v)
+assert v.eval() == 1
+
+assert isinstance(v, Expr) # ==> True
+assert isinstance(v, Val) # ==> True
+assert not isinstance(v, int)
+
+class Add(Expr):
+    __slots__=['left', 'right']
+    def __init__(self, a, b):
+        if not isinstance(a.Expr):
+            a = Val(a)
+        if not isinstance(b.Expr):
+            b = Val(b)
+        self.left = a   # aとb は式
+        self.right = b
+    def eval(self):
+        return self.left.eval() + self.right.eval()
+
+e = Add(Val(1), Val(2))  # 1+2
+assert e.eval() == 3
+
+e = Add(1,2)
+assert e.eval() == 3
+
+
+e = Add(Val(1),Add(Val(2),Val(3)))
+assert e.eval() == 6
