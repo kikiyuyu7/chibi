2b356063267924085748d838fdf4757e41e5317d
OSHIMA Yukie <21716023@jwu.ac.jp>
2019-10-28 16:47:16 +0900

    1028

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-from exp import Val, Add
+from exp import Val, Add,Sub,Mul,Div
 
 def parse(s: str):
     num = int(s)
@@ -16,14 +16,18 @@ s2 = s[pos+1:]
 print(s,s1,s2)
 '''
 def parse(s: str):
-    pos = s.find('+')
-    if pos == -1:
-        num = int(s)
-        return Val(num)
-    else:
+    if s.find('+') > 0:
+        pos = s.find('+')
         s1 = s[0:pos]
         s2 = s[pos+1:]
-        return Add(Val(int(s1)),Val(int(s2)))
+        return Add(parse(s1),parse(s2))
+    if s.find('*') > 0:
+        pos = s.find('*')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Mul(parse(s1),parse(s2))
+    return Val(int(s))
 
-e = parse("123+456+789")
-print(e)
+e = parse("1-2-3")
+print(e,e.eval())
+assert e.eval() == -4
