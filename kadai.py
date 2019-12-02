539a36b4b902d010a22dde873116e59a97c5f5ba
OSHIMA Yukie <21716023@jwu.ac.jp>
2019-12-02 15:41:25 +0900

    kadai

---
+++ kadai.py
@@ -0,0 +1,12 @@
+from time import sleep
+ 
+target_time = 10
+ 
+def up_timer(secs):
+    for i in range(0,secs):
+        print(i)
+        sleep(1)
+    print('time up!')
+ 
+ 
+up_timer(target_time)
